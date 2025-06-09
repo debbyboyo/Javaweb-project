44ce8f82eb081270189cba62bacda69a0deca3cb
debbyboyo <boyodebby@gmail.com>
2025-06-10 00:08:55 +0100

    instance ami update

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "workstation" {
-  ami           = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2023 in us-east-2
-  instance_type = "t2.small"
-  subnet_id     = aws_subnet.public_az1.id
-  key_name      = "Jesus"
-  security_groups = [aws_security_group.ssh_access.name]
+  ami                         = "ami-0f34c5ae932e6f0e4" # Amazon Linux 2023 us-east-2
+  instance_type               = "t2.small"
+  key_name                    = "Jesus"
+  subnet_id                   = aws_subnet.public_az1.id
+  associate_public_ip_address = true
 
   tags = {
     Name = "workstation"
@@ -11,25 +11,25 @@ resource "aws_instance" "workstation" {
 }
 
 resource "aws_instance" "node1" {
-  ami           = "ami-0c2b8ca1dad447f8a"
-  instance_type = "t2.small"
-  subnet_id     = aws_subnet.public_az1.id
-  key_name      = "Jesus"
-  security_groups = [aws_security_group.ssh_access.name]
+  ami                         = "ami-0f34c5ae932e6f0e4"
+  instance_type               = "t2.small"
+  key_name                    = "Jesus"
+  subnet_id                   = aws_subnet.public_az1.id
+  associate_public_ip_address = true
 
   tags = {
-    Name = "k8s-node-1"
+    Name = "k8s-node1"
   }
 }
 
 resource "aws_instance" "node2" {
-  ami           = "ami-0c2b8ca1dad447f8a"
-  instance_type = "t2.small"
-  subnet_id     = aws_subnet.public_az2.id
-  key_name      = "Jesus"
-  security_groups = [aws_security_group.ssh_access.name]
+  ami                         = "ami-0f34c5ae932e6f0e4"
+  instance_type               = "t2.small"
+  key_name                    = "Jesus"
+  subnet_id                   = aws_subnet.public_az2.id
+  associate_public_ip_address = true
 
   tags = {
-    Name = "k8s-node-2"
+    Name = "k8s-node2"
   }
 }
